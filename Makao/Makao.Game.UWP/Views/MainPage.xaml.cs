e649f78753800fbe55b66977b167c12f13178111
Konrad <konrad.lyda@outlook.com>
2016-03-10 21:29:02 +0100

    W8 projects removed, UWP project added

---
+++ Makao/Makao.Game.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Makao.Game.UWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Makao.Game.UWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
