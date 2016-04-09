aca8fed243b8a3351ce8d7abb721c9d62e1c38f1
Paweł Grzmil <pawel.grzmil@gmail.com>
2016-04-09 14:37:30 +0200

    merge

---
+++ Makao/Makao.Models/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makao.Models
+{
+    public class ChatMessage
+    {
+        public Player sender { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
